cdd83827610e4d80fd6a0359972cf864f47b9a00
ljg218 <ljg218@lehigh.edu>
2015-01-23 14:57:18 +0000

    lab01-HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,33 @@
+//Liam Gamble
+//Lab 1
+//Hello World Java Program
+//
+// first step: compile the code
+//      javac HelloWorld.java
+//
+// final step: run the program
+//      java HelloWorld
+
+// define a class...
+public class HelloWorld {
+
+//  ...then add a main method...     
+    public static void main (String[] args){
+
+// ...and finally, print the statement with a box around it.
+
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#   Hello World 2015!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("testing");
+        
+    }
+    
+}
+
+//
+//
